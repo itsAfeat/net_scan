fcbe21fd7d7827e31464384f1b41c80ff13b4928
fUNNY-fnaf <itsafeat@gmail.com>
2022-10-30 21:53:51 +0100

    Added the main two files
    color.sh includes a list of variables for the echo -e output. net_scan.sh is the main script file

---
+++ colors.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+## Bash echo colors
+# Reset
+Color_Off='\033[0m'       # Reset text
+
+# Attributes
+Bold='\033[1m'            # Bold
+Italic='\033[3m'          # Italic
+Underline='\033[4m'       # Underline
+
+# Regular Colors
+Black='\033[30m'          # Black
+Red='\033[31m'            # Red
+Green='\033[32m'          # Green
+Yellow='\033[33m'         # Yellow
+Blue='\033[34m'           # Blue
+Purple='\033[35m'         # Purple
+Cyan='\033[36m'           # Cyan
+White='\033[37m'          # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+HBlack='\033[90m'         # Black
+HRed='\033[91m'           # Red
+HGreen='\033[92m'         # Green
+HYellow='\033[93m'        # Yellow
+HBlue='\033[94m'          # Blue
+HPurple='\033[95m'        # Purple
+HCyan='\033[96m'          # Cyan
+HWhite='\033[97m'         # White
+
+# High Intensity backgrounds
+On_HBlack='\033[100m'     # Black
+On_HRed='\033[101m'       # Red
+On_HGreen='\033[102m'     # Green
+On_HYellow='\033[103m'    # Yellow
+On_HBlue='\033[104m'      # Blue
+On_HPurple='\033[105m'    # Purple
+On_HCyan='\033[106m'      # Cyan
+On_HWhite='\033[107m'     # White
