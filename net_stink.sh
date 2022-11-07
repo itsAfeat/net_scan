4c50181c15ac782305ecf5b126e9b2554d451ebe
fUNNY-fnaf <itsafeat@gmail.com>
2022-11-07 02:57:26 +0100

    Added output support
    + just some basic cleaning up... making things look nicer

--- net_stink.sh
+++ net_stink.sh
@@ -7,10 +7,27 @@ _version="0.1"
 _link="https://github.com/itsAfeat/net_stink"
 
 # Which scans to do
+do_clear=0
 do_port=0
 do_fast=0
 do_save=0
 file_name=""
+
+# Hide ^C
+stty -echoctl
+
+# Include the colors script
+DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
+. "$DIR/colors.sh"
+
+# Check if the user is root
+if [ "$EUID" -ne 0 ]; then
+    is_root=0
+else
+    is_root=1
+fi
+
 
 ##Functions
 # Help menu function
@@ -23,27 +40,21 @@ function help_menu() {
     echo -e "${Italic}${Bold}\t-p [x-y]${Color_Off}\tPort range. Same as ip range... but with ports.\n\t\t\t(ex: 443-8080)"
     echo -e "${Italic}${Bold}\t-f${Color_Off}\t\tDo a fast scan. This will make the scan faster, but\n\t\t\tit will also be less accurate."
     echo -e "${Italic}${Bold}\t-o [file]${Color_Off}\tLog that sweet scan to a file."
+    echo -e "${Italic}${Bold}\t-c${Color_Off}\t\tClear the terminal at the start of the scan."
+}
+
+# Show and hide the cursor
+function show_cursor() {
+    tput cnorm
+}
+
+function hide_cursor() {
+    tput civis
 }
 
 
-# Check if the user has sed, awk and netcat installed
-
-# Check if the user is root
-if [ "$EUID" -ne 0 ]; then
-    is_root=0
-else
-    is_root=1
-fi
-
-
-# Include the colors script
-DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
-. "$DIR/colors.sh"
-
-
 # Get all the flags and set their corresponding values
-while getopts i:p:o:fh flag
+while getopts i:p:o:fhc flag
 do
     case "${flag}" in
         i) ip_range=${OPTARG};;
@@ -51,6 +62,7 @@ do
         o) file_name=${OPTARG}; do_save=1;;
         f) do_fast=1;;
         h) help_menu; exit;;
+        c) do_clear=1;;
     esac
 done
 
@@ -64,7 +76,16 @@ open_ips=()
 open_ports=()
 declare -A ip_dict
 
-echo -e "\n${Bold}${Yellow}[!]${Color_Off} Starting host scan\n"
+if [ $do_clear -eq 1 ]; then
+    clear
+else
+    echo -e -n "\n"
+fi
+
+#trap show_cursor INT TERM
+#hide_cursor
+
+echo -e "${Bold}${Yellow}[!]${Color_Off} Starting host scan\n\n"
 
 if [ $do_fast -eq 1 ]; then
     timeout=2
@@ -73,14 +94,14 @@ else
 fi
 
 if [ $do_save -eq 0 ]; then
-    for i in {0..10}; do
-        tmp_ip=$(sed "s/*/$i/g" <<< "$ip_range")
+    for i in {0..50}; do
+        ip=$(sed "s/*/$i/g" <<< "$ip_range")
         
-        _ping=$(ping -W $timeout -c 1 $tmp_ip)
+        _ping=$(ping -W $timeout -c 1 $ip)
         result=$(echo "$_ping" | awk '/loss/ {print $6}')
+        echo -e "\e[1A\e[K> ${Italic}$ip${Color_Off}"
         if [ "$result" != "100%" ]; then
-            ip=$(echo "$_ping" | awk '/PING/ {print $2}')
-            echo -e "${Bold}${Green}[+]${Color_Off} $ip"
+            echo -e "\e[1A\e[K${Bold}${Green}[+]${Color_Off} $ip\n"
             open_ips+=("$ip")
         fi
     done
@@ -111,10 +132,49 @@ if [ $do_save -eq 0 ]; then
                 echo -e "\t${Italic}${Bold}${ip_dict[$key]}${Color_Off}\n"
             fi
         done
-        
-        exit
     fi
 else
-    echo -e "fat funny ${Bold}fart${Color_Off} moments ;)"
-    exit
+    echo "Host scan results" > $file_name
+    echo -e "--------------------------------------------------------------\n" >> $file_name
+    
+    for i in {0..50}; do
+        tmp_ip=$(sed "s/*/$i/g" <<< "$ip_range")
+        
+        _ping=$(ping -W $timeout -c 1 $tmp_ip)
+        result=$(echo "$_ping" | awk '/loss/ {print $6}')
+        if [ "$result" != "100%" ]; then
+            ip=$(echo "$_ping" | awk '/PING/ {print $2}')
+            echo "[+] $ip" >> $file_name
+            open_ips+=("$ip")
+        fi
+    done
+
+    if [ $do_port -eq 1 ]; then
+        echo -e "${Bold}${Yellow}[!]${Color_Off} Starting port scan\n"
+        for ip in "${open_ips[@]}"; do
+            open_ports=$(nc -z -vv -n $ip $port_range 2>&1 | awk '/open/ {print $3" "($4)}')
+
+            if [ -z "${open_ports[0]}" ]; then
+                ip_dict["$ip"]="-1"
+            else
+                ip_dict["$ip"]=$open_ports  
+            fi
+        done
+
+        echo -e "\n--------------------------------------------------------------" >> $file_name
+        echo -e "\n\nPort scan results" >> $file_name
+        echo -e "--------------------------------------------------------------" >> $file_name
+
+        for key in "${!ip_dict[@]}"; do
+            echo -e "\n[>] $key" >> $file_name
+            if [ "${ip_dict[$key]}" = "-1" ]; then
+                echo -e "\tNo open ports\n" >> $file_name
+            else
+                echo -e "\t{ip_dict[$key]}\n" >> $file_name 
+            fi
+        done
+    fi
 fi
+
+#show_cursor
+exit
